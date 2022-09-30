e4f1364c81904c03828a119b52dccb89f25b35ca
huangzhenjiang <huangzhenjiang@zlg.cn>
2022-09-30 11:15:22 +0800

    prereder test

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+// import path from 'path'
+import vitePrerender from 'vite-plugin-prerender'
+
+const path = require('path')
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vitePrerender({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/', '/about'],
+      })
+],
   resolve:{
     alias:{
         '@': path.resolve(__dirname, 'src')
